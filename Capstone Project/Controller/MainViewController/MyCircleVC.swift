3969e76fc0629b7760c7ed1216a9e586d117103d
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-10-19 23:42:48 +0900

    MyCircleViewController

--- Capstone Project/Controller/MainViewController/MyCircleVC.swift
+++ Capstone Project/Controller/MainViewController/MyCircleVC.swift
@@ -14,11 +14,39 @@ import UIKit
 
 class MyCircleVC: UIViewController {
 
-    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+}
+
+extension MyCircleVC: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+extension MyCircleVC: UITableViewDelegate {
+    
 }
