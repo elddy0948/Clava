4b92eb968a1dc326d3232d909241e8deae7becc8
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-10-20 23:27:05 +0900

    make header view

--- Capstone Project/Controller/MainViewController/MyCircleVC.swift
+++ Capstone Project/Controller/MainViewController/MyCircleVC.swift
@@ -32,14 +32,30 @@ class MyCircleVC: UIViewController {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
     }
+    
+    private func mockData() {
+        let circle = Circle(name: "DCA", organization: "Dong-A", description: "Welcome", circleProfilePhoto: "None", follower: [], circleMember: [], category: "Computer", post: [])
+        let dumpData = User(email: "elddy@naver.com", password: "1234", userName: "Holuck", nickName: "Holuck", gender: .male, organization: "Dong-A", birth: Date(), myCircle: [circle], followCircle: [circle], join: Date(), profilePhoto: nil)
+    }
 }
 
 extension MyCircleVC: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 2
     }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if section == 0 {
+
+        } else if section == 1 {
+
+        }
+        return UIView()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        print(section)
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
