6961f0909ea7554d15fd94f2a5f2894759d58e1e
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-10-24 23:28:47 +0900

    My Profile ViewController

--- Capstone Project/Controller/MainViewController/MyPageVC.swift
+++ Capstone Project/Controller/MainViewController/MyPageVC.swift
@@ -8,22 +8,90 @@
 import UIKit
 
 class MyPageVC: UIViewController {
-
+    
+    private var user = User(email: "elddy@naver.com", password: "1234", userName: "Holuck", nickName: "Holuck", gender: .male, organization: "Dong-A", birth: Date(), myCircle: [Circle(name: "DCA", organization: "Dong-A", description: "Welcome", circleProfilePhoto: "None", follower: [], circleMember: [], category: "Computer", post: [])], followCircle: [Circle(name: "DCA", organization: "Dong-A", description: "Welcome", circleProfilePhoto: "None", follower: [], circleMember: [], category: "Computer", post: [])], join: Date(), profilePhoto: nil)
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(MyCircleTableViewCell.self,
+                           forCellReuseIdentifier: MyCircleTableViewCell.reuseIdentifier)
+        tableView.register(FollowTableViewCell.self,
+                           forCellReuseIdentifier: FollowTableViewCell.reuseIdentifier)
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
-    */
 
+}
+
+extension MyPageVC: UITableViewDelegate {
+    
+}
+
+extension MyPageVC: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = UIView(frame: CGRect(x: 0, y: 0, width: tableView.width, height: 70))
+        let label = UILabel(frame: CGRect(x: 2, y: 2, width: header.width, height: header.height))
+        header.addSubview(label)
+        header.backgroundColor = .secondarySystemBackground
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.textColor = .label
+        if section == 0 {
+            label.text = "My Circle"
+        } else if section == 1 {
+            label.text = "Follow Circle"
+        }
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 70
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //section 0 ==> My Circle
+        //section 1 ==> Follow Circle
+        if section == 0 {
+            return user.myCircle?.count ?? 0
+        } else if section == 1 {
+            return user.followCircle?.count ?? 0
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: MyCircleTableViewCell.reuseIdentifier,
+                                                           for: indexPath) as? MyCircleTableViewCell else {
+                fatalError("My Circle TableviewCell Error!")
+            }
+            tableView.separatorStyle = .none
+            return cell
+        } else if indexPath.section == 1 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: FollowTableViewCell.reuseIdentifier,
+                                                           for: indexPath) as? FollowTableViewCell else {
+                fatalError("Follow TableviewCell Error!")
+            }
+            tableView.separatorStyle = .none
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    
 }
