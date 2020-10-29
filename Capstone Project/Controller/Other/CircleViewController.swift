90bd31a8be1ab1c22cef7c451a3df30f54bfb3f8
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-10-30 03:45:47 +0900

    making CircleViewController UI

--- Capstone Project/Controller/Other/CircleViewController.swift
+++ Capstone Project/Controller/Other/CircleViewController.swift
@@ -12,11 +12,30 @@ import UIKit
 // 소속 위치 카테고리
 // 설명
 
+/*
+ -  Header
+    - 동아리 이미지
+    - 소속  / 위치  / 카테고리
+    - 설명
+ - Posts
+    -Post Cell
+ */
+
 class CircleViewController: UIViewController {
+    
+    private let user = User(email: "holuck@naver.com", password: "123456", userName: "Hojoon", nickName: "Holuck",
+                            gender: .male, organization: "Dong-A Univ", birth: Date(), myCircle: [Circle(name: "DCA", organization: "Dong-A Univ", description: "Computer Circle", circleProfilePhoto: "", follower: [], circleMember: [], category: "Computer", post: [])], followCircle: [], join: Date(), profilePhoto: nil)
     
     private let tableView: UITableView = {
         let tableView = UITableView()
-        
+        tableView.register(FeedHeaderTableViewCell.self,
+                           forCellReuseIdentifier: FeedHeaderTableViewCell.reuseIdentifier)
+        tableView.register(FeedPostTableViewCell.self,
+                           forCellReuseIdentifier: FeedPostTableViewCell.reuseIdentifier)
+        tableView.register(FeedActionTableViewCell.self,
+                           forCellReuseIdentifier: FeedActionTableViewCell.reuseIdentifier)
+        tableView.register(FeedCommentTableViewCell.self,
+                           forCellReuseIdentifier: FeedCommentTableViewCell.reuseIdentifier)
         return tableView
     }()
 
@@ -24,24 +43,114 @@ class CircleViewController: UIViewController {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        view.addSubview(tableView)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
 }
 
 extension CircleViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        //MARK: - Bring The Model
+        guard let myCircle = user.myCircle else {fatalError("Can't Get MyCircle")}
+        let model = myCircle[indexPath.section / 4]
+        
+        if indexPath.section == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: FeedHeaderTableViewCell.reuseIdentifier,
+                                                               for: indexPath) as? FeedHeaderTableViewCell else {
+                fatalError("FeedHeaderTableViewCell")
+            }
+            
+            // FeedHeaderTableViewCell Delegate 채택
+            cell.delegate = self
+            cell.configure(model: model)
+            cell.selectionStyle = .none
+            return cell
+        }
+        else if indexPath.section == 1 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: FeedPostTableViewCell.reuseIdentifier,
+                                                               for: indexPath) as? FeedPostTableViewCell else {
+                fatalError("FeedPostTableViewCell")
+            }
+            cell.selectionStyle = .none
+            return cell
+        }
+        else if indexPath.section == 2 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: FeedActionTableViewCell.reuseIdentifier,
+                                                               for: indexPath) as? FeedActionTableViewCell else {
+                fatalError("FeedActionTableViewCell")
+            }
+            cell.selectionStyle = .none
+            return cell
+        }
+        else if indexPath.section == 3 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: FeedCommentTableViewCell.reuseIdentifier,
+                                                               for: indexPath) as? FeedCommentTableViewCell else {
+                fatalError("FeedCommentTableViewCell Error")
+            }
+            cell.selectionStyle = .none
+            return cell
+        }
         return UITableViewCell()
+        
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return 60
+        } else if indexPath.section == 1 {
+            return tableView.width
+        } else if indexPath.section == 2 {
+            return 60
+        } else if indexPath.section == 3 {
+            return 80
+        }
+        return 0
+    }
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        if section % 4 == 3 {
+            return UIView()
+        }
+        
+        return nil
+    }
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        if section % 4 == 3 {
+            return 50
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if section == 0 {
+            return CircleHeaderView()
+        }
+        return nil
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return view.width
+        }
+        return 0
     }
 }
 
-
+extension CircleViewController: FeedHeaderTableViewCellDelegate {
+    func pressProfileName() {
+    }    
+}
 
 
 
--- Capstone Project/View/Cell/CircleCell/CircleHeaderView.swift
+++ Capstone Project/View/Cell/CircleCell/CircleHeaderView.swift
@@ -8,10 +8,53 @@
 import UIKit
 
 class CircleHeaderView: UIView {
-
+    
+    private let circleImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        return imageView
+    }()
+    
+    private let circleBelong: UILabel = {
+        let label = UILabel()
+        label.text = "Dong-A Univ"
+        label.numberOfLines = 1
+        label.textColor = .label
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let circleArea: UILabel = {
+        let label = UILabel()
+        label.text = "Pusan"
+        label.numberOfLines = 1
+        label.textAlignment = .center
+        label.textColor = .label
+        return label
+    }()
+    
+    private let circleCategory: UILabel = {
+        let label = UILabel()
+        label.text = "Security"
+        label.numberOfLines = 1
+        label.textAlignment = .center
+        label.textColor = .label
+        return label
+    }()
+    private let circleDescription: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .red
+        return textView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+        backgroundColor = .yellow
+        self.addSubview(circleImageView)
+        self.addSubview(circleArea)
+        self.addSubview(circleBelong)
+        self.addSubview(circleCategory)
+        self.addSubview(circleDescription)
     }
     
     required init?(coder: NSCoder) {
@@ -20,5 +63,17 @@ class CircleHeaderView: UIView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        let size = self.width / 3
+        circleImageView.frame = CGRect(x: size, y: 10, width: size, height: size)
+        circleImageView.layer.cornerRadius = size / 2
+        
+        let labelSize = self.width / 3
+        let heightFromImage = circleImageView.bottom + 8
+        circleBelong.frame = CGRect(x: 0, y: heightFromImage, width: labelSize, height: 30)
+        circleArea.frame = CGRect(x: circleBelong.right, y: heightFromImage, width: labelSize, height: 30)
+        circleCategory.frame = CGRect(x: circleArea.right, y: heightFromImage, width: labelSize, height: 30)
+        
+        circleDescription.frame = CGRect(x: 0, y: circleCategory.bottom + 8, width: self.width, height: self.height - circleImageView.height - 56)
+        
     }
 }
